87a7dda7dba05016d364298867dc80f6ecc20f82
Laura <laurakriley01@gmail.com>
2021-02-04 08:01:29 -0700

    Add comments

--- MultiplicationTable.vb
+++ MultiplicationTable.vb
@@ -1,4 +1,12 @@
-﻿Module MultiplicationTable
+﻿'Laura Riley
+'RCET0265
+'Spring 2021
+'Multiplication Table
+'https://github.com/rilelaur/MultiplicationTable.git
+
+Option Explicit On
+Option Strict On
+Module MultiplicationTable
 
     Sub Main()
         Dim userInput As String = " "
@@ -6,6 +14,9 @@
         Dim tableValue As Integer
         Dim problem As Boolean = True
 
+        'Asks the user for a number and makes sure they enter a valid number
+        'If the user enters anything but a number it will tell them they are 
+        'wrong and ask for a number
         While problem = True
             Console.WriteLine("Please enter a number.")
             Try
@@ -18,6 +29,7 @@
             End Try
         End While
 
+        'Writes out the multiplication table after the user enters a number
         If problem = False Then
             Console.WriteLine(vbCrLf & $"Here is your {tableValue} by {tableValue} multiplication table." & vbCrLf)
             For i = 1 To (tableValue)
